04cca704a727d0d972912703bcbcddf3819f66f3
onaumenko-pixel <o.naumenko@rayton.com.ua>
2026-02-25 12:43:35 +0100

    Update crm_lead.py

--- crm_lead_contacts/models/crm_lead.py
+++ crm_lead_contacts/models/crm_lead.py
@@ -5,21 +5,7 @@ class CrmLead(models.Model):
     _inherit = 'crm.lead'
 
     contact_ids = fields.One2many(
-        comodel_name='res.partner',
-        inverse_name='parent_id',
+        related='partner_id.child_ids',
         string='Contacts',
-        domain="[('type', '=', 'contact')]",
-        compute='_compute_contact_ids',
         readonly=False,
-        store=False,
     )
-
-    @api.depends('partner_id', 'partner_id.child_ids', 'partner_id.child_ids.type')
-    def _compute_contact_ids(self):
-        for lead in self:
-            if lead.partner_id:
-                lead.contact_ids = lead.partner_id.child_ids.filtered(
-                    lambda p: p.type == 'contact'
-                )
-            else:
-                lead.contact_ids = self.env['res.partner']
